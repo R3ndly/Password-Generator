970437bb4394b2b4a9ad4e4eb92c5acc52f81851
R3ndly <akalinart020@gmail.com>
2024-08-20 20:41:59 +0800

    Мой первый commit

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         $haveNumbers = $_POST["numbers"];
         $haveUpperCase = $_POST["uppercase"];
         $haveSymbol = $_POST["symbol"];
-
+//123
         $letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
         $numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
         $symbols = ['$', '!', '%', '*', '_', '@', '#', '№'];
